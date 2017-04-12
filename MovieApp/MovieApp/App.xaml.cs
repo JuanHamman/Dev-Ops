7600082e06954224be257e9756728d06996ebf4e
Juan Hamman <Juan Hamman@JUANLAPTOP>
2017-04-12 13:15:59 +0200

    Added whole new project

--- MovieApp/MovieApp/App.xaml.cs
+++ MovieApp/MovieApp/App.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace MovieApp
 {
@@ -13,7 +8,7 @@ namespace MovieApp
         {
             InitializeComponent();
 
-            MainPage = new MovieApp.MainPage();
+            MainPage = new NavigationPage(new MoviesPage());
         }
 
         protected override void OnStart()
